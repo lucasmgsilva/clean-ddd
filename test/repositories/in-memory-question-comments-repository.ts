301ded2245293324f7b446fb15b6d40e27566ab2
Lucas Matheus <lucasmgsilva@hotmail.com>
2024-05-31 10:26:43 -0300

    caso de uso: deletar comentário da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,19 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async findById(id: string) {
+    return this.items.find((item) => item.id.toString() === id) || null
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
