d04185d40ba8b2c099b56edaf2680166e8c447df
Lucas Matheus <lucasmgsilva@hotmail.com>
2024-08-28 18:47:44 -0300

    evento: resposta criada

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
-import { AnswerAttachmentList } from './answer-attachment-list'
+import { AnswerAttachmentList } from '@/domain/forum/enterprise/entities/answer-attachment-list'
+import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
 
 export interface AnswerProps {
   authorId: UniqueEntityID
@@ -12,7 +13,7 @@ export interface AnswerProps {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   get authorId() {
     return this.props.authorId
   }
@@ -52,6 +53,7 @@ export class Answer extends Entity<AnswerProps> {
 
   set attachments(attachments: AnswerAttachmentList) {
     this.props.attachments = attachments
+    this.touch()
   }
 
   static create(
@@ -66,6 +68,12 @@ export class Answer extends Entity<AnswerProps> {
       },
       id,
     )
+
+    const isNewAnswer = !id
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCreatedEvent(answer))
+    }
 
     return answer
   }
